fc205b11b60877ca9fe46a11eb5e827714926059
Florian Veaux <florian.veaux@datadoghq.com>
2022-03-28 10:52:29 -0400

    Update file version

--- omnibus/config/software/snmp-traps.rb
+++ omnibus/config/software/snmp-traps.rb
@@ -1,8 +1,8 @@
 name "snmp-traps"
-default_version "0.1.0"
+default_version "0.2.0"
 
 source :url => "https://s3.amazonaws.com/dd-agent-omnibus/snmp_traps_db/dd_traps_db-#{version}.json.gz",
-       :sha256 => "69c3ea4e3898b6889f951a9a6768339635de06b5a9ff8be85825d6cbe08e28f0",
+       :sha256 => "dd308ba8ec1453ed73d60e9b8d4c38050371fdceaab4404448e1084d628d3298",
        :target_filename => "dd_traps_db.json.gz"
 
 
